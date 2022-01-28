c473685e849cd8207267cd7fb899100889858c99
Maksym Bernatskyi <bernasik007@gmail.com>
2022-01-28 14:46:02 +0200

    HW-08 task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const STORAGE_KEY = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+let formData = {};
+
+const onPlay = function (data) {
+    formData = data;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    };
+
+    player.on('timeupdate', throttle(onPlay, 1000));
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+const getLocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+player.setCurrentTime(getLocalStorage.seconds).then(function (seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
