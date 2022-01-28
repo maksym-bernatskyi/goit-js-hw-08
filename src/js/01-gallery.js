d9d89f49fb64ae35ef5c9cb0c894be29177ae08f
Maksym Bernatskyi <bernasik007@gmail.com>
2022-01-28 14:04:21 +0200

    HW-08 task-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,15 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const mainGalleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createGalleryItemsMarkup = galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}" onclick="event.preventDefault()">
+  <img class="gallery__image" src="${preview}"  alt="Image description" title="${description}"/>
+</a>`).join('');
+
+mainGalleryList.innerHTML = createGalleryItemsMarkup;
+
+let lightbox =  new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 500, captionPosition: 'top', });
